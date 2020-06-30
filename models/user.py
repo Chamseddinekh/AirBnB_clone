4300a6b969ebc5bf4ff5d36124e09981d3409ae2
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-06-30 16:01:09 +0100

    create user

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" base class for all """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """class user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
