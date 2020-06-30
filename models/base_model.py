a2743a24e6d5ffee47ab4179b432db2ab22a2cc4
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-06-30 15:53:26 +0100

    update kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,16 @@ import datetime
 class BaseModel():
     """Basemodel constructor"""
 
-
     def __init__(self, *args, **kwargs):
         if kwargs:
             for arg in kwargs:
                 if arg == "created_at":
-                    self.created_at = datetime.datetime.strptime(
-                        self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
-                elif arg == "updated_at":
-                    self.updated_at = datetime.datetime.strptime(
-                        self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[arg] = kwargs[arg]
+                    if arg != "__class__":
+                        self.__dict__[arg] = kwargs[arg]
+                self.created_at = datetime.datetime.strptime(
+                    self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+                self.updated_at = datetime.datetime.strptime(
+                    self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
@@ -28,24 +26,18 @@ class BaseModel():
         """
         BaseModel str method
         """
-
-
         return ("{}{}{}".format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
         """
         public instance attribute updated_at with the current datetime
         """
-
-
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
         Method eturns a dictionary containing all keys/values
         """
-
-
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = type(self).__name__
         new_dict['created_at'] = new_dict["created_at"].isoformat()
