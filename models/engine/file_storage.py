4542dd91af3117175617bca8a3355daac1979f36
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-21 00:01:59 +0100

    all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 module for serializing and deserializing object to file storage
 """
 import os
+import json
 
 
 class FileStorage():
@@ -24,3 +25,12 @@ class FileStorage():
         """sets in the obj with key <obj class name>.id"""
         key = str(__class__.__name__)+"."+str(obj.id)
         value = obj
+        FileStorage.__objects[key] = value
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict = value.to_dict()
+        with open(FileStorage.__file_path, mode='w', encoding='UTF8') as f:
+            json.dump(new_dict, f)
