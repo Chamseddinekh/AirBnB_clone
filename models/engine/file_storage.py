7a5663c05423258f377df9ed68e0756107c981d7
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-20 22:09:37 +0100

    all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,3 +2,20 @@
 """
 module for serializing and deserializing object to file storage
 """
+import os
+
+
+class FileStorage():
+    """
+    FileStorage class for serializing and deserializing objects
+    into and from files respectively
+    """
+
+    engine_directory = os.path.dirname(os.path.abspath(__file__))
+    parent_directory = os.getcwd()
+    __file_path = parent_directory + '/file.json'
+    __objects = dict()
+
+    def all(self):
+        """returns the dictionary"""
+        return self.__objects
