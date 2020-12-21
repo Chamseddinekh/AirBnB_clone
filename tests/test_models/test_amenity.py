b3a61b22c3a41323421fd415e03f5a344f7ac09b
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-21 01:20:06 +0100

    testamenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,3 +2,15 @@
 """
 test module for testing amenity models
 """
+import unittest
+import os
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestCity(unittest.TestCase):
+    """test the city class"""
+
+    def test_subclass_amenity(self):
+        """amenity subclass test"""
+        self.assertTrue(issubclass(self.amenity.__class__, BaseModel), True)
